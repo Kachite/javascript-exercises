bb9fcd1d060e84654e030e9722c4e7a6542e6bd7
Uzomba Onyekachi Victor <victor.uzomba@yahoo.com>
2024-08-25 19:25:10 +0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (initial, final) {
+  if (!Number.isInteger(initial) || !Number.isInteger(final)) {
+    return "ERROR";
+  }
+  else if (initial < 0 || final <0){
+    return "ERROR";
+  }
+  else if (initial > final) {
+    let sum = final;
+    for (i = final; i < initial; i++) {
+      final++;
+      sum += final;
+    }
+    return sum;
+  } else {
+    let sum = initial;
+    for (let i = initial; i < final; i++) {
+      initial++;
+      sum += initial;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
