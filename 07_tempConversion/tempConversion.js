0ea714c54271fc92727c3c41ebe1f9ac100ffcce
Uzomba Onyekachi Victor <victor.uzomba@yahoo.com>
2024-08-26 15:15:31 +0300

    Solve exercise 07 on temperature conversion using the formular for conversion and also converting the result to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  let rounded = Math.round(((x - 32) * (5 / 9)) * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  let rounded = Math.round((x * (9 / 5) + 32) * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
